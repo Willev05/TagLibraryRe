ab71395a351b376318004183dda438e2e1460bf2
William <willev300@gmail.com>
2025-12-19 01:41:30 -0500

    Synching work to github

--- TagLibraryRe/MainWindow.xaml.cs
+++ TagLibraryRe/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Interop;
+
 using TagLibraryRe.ViewModel;
 
 namespace TagLibraryRe
@@ -10,6 +12,17 @@ namespace TagLibraryRe
             InitializeComponent();
             MainWindowViewModel vm = new MainWindowViewModel();
             DataContext = vm;
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+            var workArea = SystemParameters.WorkArea;
+
+            Width = workArea.Width * 0.8;
+            Height = workArea.Height * 0.8;
+
+            Left = workArea.Left + (workArea.Width - Width) / 2;
+            Top = workArea.Top + (workArea.Height - Height) / 2;
         }
     }
 }
